c2461b8bd6089d9de6bf4f3b0a4e33d688eb502e
khalidlotfi <khalidlotfi2005@gmail.com>
2023-09-13 18:02:15 +0100

    adding user in the seeds
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-puts "Seeding user"
-User.create!(email: "test@gmail.com", password: "test123", password_confirmation: "test123")
-
+puts "Destroying all Users"
+User.destroy_all
 puts "Destroying all Posts"
 Post.destroy_all
 
-puts "Creating Posts"
+puts "Seeding user"
+User.create!(email: "test@gmail.com", name: "test", password: "test123", password_confirmation: "test123")
+User.create!(email: "zorro@example.com", name: "zorro", password: "password", password_confirmation: "password")
+
+puts "Seeding Posts"
 10.times do |i|
   Post.create!(title: "first #{i + 1}", body: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptatum illo voluptates officia animi rem aliquam earum repellat? Sed nemo asperiores velit doloribus praesentium nihil atque illo aliquam fugiat facere. #{i + 1}", user_id: User.first.id)
 end
