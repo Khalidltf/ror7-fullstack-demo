6eee393f54a52d9a01e1c872ca913f400066b3a0
khalidlotfi <khalidlotfi2005@gmail.com>
2023-09-27 09:24:22 +0100

    Setup Initial Ransack and Ransack Search Bar Form
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'search', to: 'search#index'
   # get 'users/profile'
 
   devise_for :users, controllers: {
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  
+
   get 'a', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
